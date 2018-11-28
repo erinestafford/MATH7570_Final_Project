c25a7176a9122a35a06561f1902be4440574ec70
Erin Stafford <estaffor@tulane.edu>
2018-11-27 20:05:24 -0600

    jacobi solver

--- main_Poisson_code.m
+++ main_Poisson_code.m
@@ -1,7 +1,11 @@
-n = 5;
-I = eye(n-1);
-T = diag(-4*ones(n-1,1)) + diag(ones(n-2,1),-1)+ diag(ones(n-2,1),1);
-A = [T  I zeros(size(I)) zeros(size(I)); I T I zeros(size(I)); zeros(size(I)) I T I;zeros(size(I)) zeros(size(I)) I T];
+n = 9;
+u_1 = @(x,y) x.*(x-1).*y.*(y-1);
+d2u_1 = @(x,y) 2*x.^2 + 2*y.^2 - 2*x - 2*y;
+u_2 = @(x,y) sin(2*pi.*x).*sin(2*pi.*y);
 
-u_1 = @(x,y) x*(x-1)*y*(y-1);
-u_2 = @(x,y) sin(2*pi*x)*sin(2*pi*y);
+x = 0:1/(n-1):1;
+y = 0:1/(n-1):1;
+[X,Y] = meshgrid(x,y);
+rhs = d2u_1(X,Y);
+jacobi_solve(n,rhs)
+u_1(X,Y)
