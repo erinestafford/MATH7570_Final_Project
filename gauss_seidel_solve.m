24d4e0de1521ae6a98ecd1fc16a881ebf8dacb8a
Erin Stafford <estaffor@tulane.edu>
2018-12-03 19:27:14 -0600

    figures and add BCs

--- gauss_seidel_solve.m
+++ gauss_seidel_solve.m
@@ -10,10 +10,21 @@ norm_rhs = norm(rhs(:),2);
 %% Update x
 tol = 1e-6;
 while 1
-    for i = 2:np+1
-        for j = 2:np+1
-            x_new(i,j) = (1/4)*(x_new(i-1,j)+x(i+1,j) + x(i,j-1) + x_new(i,j+1) - h^2 *rhs(i,j));
-            r(i,j) = rhs(i,j) - (1/h^2)*(x_new(i-1,j)+x(i+1,j) + x(i,j-1) + x_new(i,j+1)-4*x(i,j));
+    for i = 1:np+2
+        for j = 1:np+2
+            %Apply BCs
+            if(i ==1)
+                x_new(i,j) = x0(j);
+            elseif(i==np+2)
+                x_new(i,j) = x1(j);
+            elseif(j==1)
+                x_new(i,j) = y0(i);
+            elseif(j==np+2)
+                x_new(i,j) = y1(i);
+            else
+                x_new(i,j) = (1/4)*(x_new(i-1,j)+x(i+1,j) + x(i,j-1) + x_new(i,j+1) - h^2 *rhs(i,j));
+                r(i,j) = rhs(i,j) - (1/h^2)*(x_new(i-1,j)+x(i+1,j) + x(i,j-1) + x_new(i,j+1)-4*x(i,j));
+            end
         end
     end
     if norm(r(:),2)/norm_rhs < tol
--- jacobi_solve.m
+++ jacobi_solve.m
@@ -1,6 +1,7 @@
-function x = jacobi_solve(np, rhs)
+function x = jacobi_solve(np, rhs,x0,x1,y0,y1)
 % np - number of grid points
 % rhs - matrix of values of 2nd derivative
+% x0,x1,y0,y1 - the boundary conditions
 tol = 1e-6;
 h = 1/(np+1);
 x = zeros(np+2,np+2);
@@ -10,10 +11,21 @@ norm_rhs = norm( rhs(:),2);
 %% Update x
 tol = 1e-6;
 while 1
-    for i = 2:np+1
-        for j = 2:np+1
-            x_new(i,j) = (1/4)*(x(i-1,j)+x(i+1,j) + x(i,j-1) + x(i,j+1) - h^2 *rhs(i,j));
-            r(i,j) = rhs(i,j) - (1/h^2)*(x(i-1,j)+x(i+1,j) + x(i,j-1) + x(i,j+1)-4*x(i,j));
+    for i = 1:np+2 %x
+        for j = 1:np+2 %y
+            %Apply BCs
+            if(i ==1)
+                x_new(i,j) = x0(j);
+            elseif(i==np+2)
+                x_new(i,j) = x1(j);
+            elseif(j==1)
+                x_new(i,j) = y0(i);
+            elseif(j==np+2)
+                x_new(i,j) = y1(i);
+            else
+                x_new(i,j) = (1/4)*(x(i-1,j)+x(i+1,j) + x(i,j-1) + x(i,j+1) - h^2 *rhs(i,j));
+                r(i,j) = rhs(i,j) - (1/h^2)*(x(i-1,j)+x(i+1,j) + x(i,j-1) + x(i,j+1)-4*x(i,j));
+            end
         end
     end
     if norm(r(:),2)/norm_rhs < tol
